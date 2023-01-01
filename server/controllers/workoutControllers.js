aedfcf5987fd4d5012967fae06c8fd53b24b1660
Liban Jama <libanjama45@gmail.com>
2023-01-01 04:48:54 -0800

    completed controller/routes

--- server/controllers/workoutControllers.js
+++ server/controllers/workoutControllers.js
@@ -8,7 +8,7 @@ export const getAllWorkouts = async (req, res) => {
   
   res.status(200).json(workouts);
 
-}
+};
 
 // Get a SINGLE Workout
 export const getSingleWorkout = async (req, res) =>{
@@ -17,14 +17,14 @@ export const getSingleWorkout = async (req, res) =>{
   if (!mongoose.Types.ObjectId.isValid(id)){
     return res.status(404).json({error: "No Such Workout"});
   }
-  
+
   const workout = await myWorkoutModel.findById(id)
   if (!workout) {
     return  res.status(404).json({error: "Workout does not exist!"});
   }
 
   res.status(200).json(workout);
-}
+};
 
 // CREATE a new Workout
 export const createNewWorkout = async (req, res) => {
@@ -41,6 +41,40 @@ export const createNewWorkout = async (req, res) => {
 };
 
 // DELETE a new Workout 
+export const deleteWorkout = async (req, res) => {
+  const {id}  = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error: "No Such Workout"});
+  }
+  
+  const workout = await myWorkoutModel.findOneAndDelete({_id: id});
+
+  if (!workout) {
+    return  res.status(404).json({error: "Workout does not exist!"});
+  }
+
+  res.status(200).json(workout);
+};
 
 // UPDATE a new Workout
+export const updateWorkout = async (req, res) => {
+
+  const {id}  = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error: "No Such Workout"});
+  }
+  
+  const workout = await myWorkoutModel.findOneAndUpdate({_id: id}, { // second argument is object for changes we want to make
+    ...req.body //updating with body of the reqest and we spread it out
+
+  });
+
+  if (!workout) {
+    return  res.status(404).json({error: "Workout does not exist!"});
+  }
+
+  res.status(200).json(workout);
+
+};
--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createNewWorkout, getAllWorkouts, getSingleWorkout} from '../controllers/workoutControllers.js';
+import { createNewWorkout, getAllWorkouts, getSingleWorkout, deleteWorkout, updateWorkout} from '../controllers/workoutControllers.js';
 const router = express.Router();
 //GET all the workouts
 router.get('/', getAllWorkouts);
@@ -8,21 +8,14 @@ router.get('/', getAllWorkouts);
 router.get('/:id', getSingleWorkout );
 
 //POST a new workout
-
 router.post('/', createNewWorkout );
 
 //DELETE a new workout
-
-router.delete('/:id', (req, res) => {
-  res.json({mssg: "DELETE a single workout"});
-});
+router.delete('/:id',deleteWorkout);
 
 
 //UPDATE a new workout
-
-router.patch('/:id', (req, res) => {
-  res.json({mssg: "UPDATE a single workout"});
-});
+router.patch('/:id', updateWorkout);
 
 export {router};
 
